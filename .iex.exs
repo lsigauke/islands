0a8bb63f55e70e5b8224c38c200b136e620fd98e
Lee Sigauke <leesigs@gmail.com>
2018-05-28 22:41:25 +0100

    Complete game API

--- .iex.exs
+++ .iex.exs
@@ -1,1 +1,2 @@
-alias IslandsEngine.{Board, Coordinate, Guesses, Island, Rules}
+alias IslandsEngine.{Board, Coordinate, Game, Guesses, Island, Rules}
+i_types = [square: {5, 5}, atoll: {1, 1}, dot: {1, 4}, l_shape: {1, 5}, s_shape: {5, 1}]
